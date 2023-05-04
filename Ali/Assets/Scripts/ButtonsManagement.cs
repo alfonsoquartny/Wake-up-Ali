bbcbee58a1f3c80ea01e81b9a5fd0643d55119e7
emrecds <seyrelmeyen2@outlook.com>
2023-05-04 23:19:01 +0300

    fixes

---
+++ Ali/Assets/Scripts/ButtonsManagement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+public class ButtonsManagement : MonoBehaviour
+{
+    private GameObject pointEnterSoundEffect;
+    public AudioClip pointEnterSound;
+    public GameObject canvasPrefab;
+    void Start()
+    {
+        pointEnterSoundEffect = GameObject.FindGameObjectWithTag("hover");
+
+        Instantiate(canvasPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneManager.LoadScene("story");
+    }
+    public void YeniOyun()
+    {
+       
+    }
+    public void HoverSound()
+    {
+        Debug.Log("Pointer Entered");
+            pointEnterSoundEffect.GetComponent<AudioSource>().PlayOneShot(pointEnterSound);
+    }
+
+
+}
